0155ab1abb545aafd4f8961c10a6e3bb0ac92288
EduardoMartino763 <dudumartino4@gmail.com>
2025-10-31 10:39:24 -0300

    Salva

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
 var APIKEY = "a65dbb2240a449bd8e7183620242804";
 
+function addCidadeCard(event){
+    console.log(event);
+    let cidadeNome = document.getElementById("txtCidade").value;
+    alert(cidadeNome);
+}
+
 function populaCard(event){
     let dadosClima = JSON.parse(this.responseText);
-    console.log(dadosClima);
+    let cityCard = document.getElementById(dadosClima.location.name + "Card");
+    let cityName = cityCard.querySelector(".cityName");
+    cityName.innerHTML = dadosClima.location.name;
+    let cityTemp = cityCard.querySelector(".temp");
+    cityTemp.innerHTML = dadosClima.current.temp_c + " C";
+    let cityCondition = cityCard.querySelector(".condition");
+    cityCondition.innerHTML = '<span>Clima<\span><span>'+dadosClima.current.condition.text+'<\span>';
+    let citySensation = cityCard.querySelector(".sensacao");
+    citySensation.innerHTML = '<span>Sensação Térmica</span><span>'+dadosClima.current.feelslike_c+'&deg;C</span>';
+    //console.log(dadosClima);
 }
 
 function buscarDadosClima(cidade){
@@ -12,8 +27,11 @@ function buscarDadosClima(cidade){
     req.send();
 }
 
-buscarDadosClima("São Paulo");
-buscarDadosClima("Rio de Janeiro");
-buscarDadosClima("Belo Horizonte");
-buscarDadosClima("Marília");
-buscarDadosClima("Assis");
+function carregarSite(){
+    let cidades = document.getElementsByClassName("cityName");
+    for (let i = 0;i < cidades.length;i++){
+        buscarDadosClima(cidades[i].innerHTML);
+    }
+    let btnAddCidade = document.getElementById("btnAddCity");
+    btnAddCidade.addEventListener('click',addCidadeCard);
+}
