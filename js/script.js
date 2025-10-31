6fcb4510bc030cdc065016d131e192a3c36fe8d7
EduardoMartino763 <dudumartino4@gmail.com>
2025-10-31 10:52:27 -0300

    salvo

--- js/script.js
+++ js/script.js
@@ -3,7 +3,10 @@ var APIKEY = "a65dbb2240a449bd8e7183620242804";
 function addCidadeCard(event){
     console.log(event);
     let cidadeNome = document.getElementById("txtCidade").value;
-    alert(cidadeNome);
+    let mainListCards = document.getElementsByTagName("main")[0];
+    let newCard = '<div class="wheaterCard" id="'+cidadeNome+'Card"><header class="cardHeader"><h2>Tempo agora em<br ><span class="cityName">Marília</span></h2></header><div class="cityData"><p class="temp">30.6&deg;</p><br ><p class="itemsCD condition"><span>Clima</span><span>Ensolarado</span></p><p class="itemsCD sensacao"><span>Sensação Térmica</span><span>31.0</span></p></div></div>';
+    mainListCards.innerHTML = mainListCards.innerHTML + newCard;
+    buscarDadosClima(cidadeNome);
 }
 
 function populaCard(event){
@@ -12,11 +15,11 @@ function populaCard(event){
     let cityName = cityCard.querySelector(".cityName");
     cityName.innerHTML = dadosClima.location.name;
     let cityTemp = cityCard.querySelector(".temp");
-    cityTemp.innerHTML = dadosClima.current.temp_c + " C";
+    cityTemp.innerHTML = dadosClima.current.temp_c + " °C";
     let cityCondition = cityCard.querySelector(".condition");
-    cityCondition.innerHTML = '<span>Clima<\span><span>'+dadosClima.current.condition.text+'<\span>';
+    cityCondition.innerHTML = '<span>Clima</span><span>'+dadosClima.current.condition.text+'</span>';
     let citySensation = cityCard.querySelector(".sensacao");
-    citySensation.innerHTML = '<span>Sensação Térmica</span><span>'+dadosClima.current.feelslike_c+'&deg;C</span>';
+    citySensation.innerHTML = '<span>Sensação Térmica</span><span>'+dadosClima.current.feelslike_c+' &deg;C</span>';
     //console.log(dadosClima);
 }
 
